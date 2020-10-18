f0c82f06689351e0c8f70025dc49903ec850e7b7
vecmty <ar2r1993@gmail.com>
2020-10-18 14:06:45 +0600

    Delete src/main/java/com/vecmty/firstProject/repository/UserRepository.java

--- src/main/java/com/vecmty/firstProject/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vecmty.firstProject.repository;
-
-import com.vecmty.firstProject.model.ApplicationUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
-    ApplicationUser findByUsername(String name);
-}
